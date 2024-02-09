7f4dbfc3bc6363d1033da964622431f63894ccd3
kimatron <kimmykim@gmail.com>
2024-02-09 23:01:15 +0000

    Add error test for API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,10 @@ const API_KEY = "I9Ne1whaBKBycM3831LqfoEWnbE";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document.getElementById("status").addEventListener("click", e => {
-  getStatus(e)
-});
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
+
   const queryString = `${API_URL}?api_key=${API_KEY}`;
 
   const response = await fetch(queryString);
@@ -14,6 +13,9 @@ async function getStatus(e) {
   const data = await response.json();
 
   if (response.ok) {
-    console.log(data);
+    console.log(data.expiry);
+  } else {
+    throw new Error(data.error);
   }
+
 }
